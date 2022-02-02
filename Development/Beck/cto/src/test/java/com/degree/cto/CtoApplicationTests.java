c75b26625ec0312d467d5c89910eec6352967f63
Okhotnik-V <70792766+Okhotnik-V@users.noreply.github.com>
2022-02-02 13:12:11 +0200

    initialize the project

---
+++ Development/Beck/cto/src/test/java/com/degree/cto/CtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degree.cto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
